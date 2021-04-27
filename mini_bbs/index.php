4a1e8794b0b87fdd9467e8a78cf0163d73fdcb8d
yutakikkawa <yutakikkawa@localhost.localdomain>
2021-04-27 15:11:24 +0900

    Correction of deletion function

--- mini_bbs/index.php
+++ mini_bbs/index.php
@@ -132,8 +132,7 @@ function makeLink($value) {
                     <a href="view.php?id=<?php echo h($post['reply_post_id']) ?>">返信元のメッセージ</a>
                 <?php endif ?>
                 <?php if ($_SESSION['id'] == $post['member_id']): ?>
-                    [<a href="#" onclick="Confirm();" style="color: #F33;" >削除</a>]
-                    <a id="delete" href="delete.php?id=<?php echo h($post['id'])?>"></a>
+                    [<a href="delete.php?id=<?php echo h($post['id'])?>" style="color: #F33;" >削除</a>]
                 <?php endif ?>
             </p>
         </div>
